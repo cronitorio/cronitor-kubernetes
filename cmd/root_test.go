9fd049269df03e6aaf6ae30bff349a4f8a0f8732
jj <jj@jdotjdot.com>
2022-07-10 23:37:48 +0200

    Add checks for invalid API keys provided

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"os"
 	"testing"
 )
 
@@ -15,5 +17,27 @@ func TestLogLevelParsing(t *testing.T) {
 				t.Error(err)
 			}
 		})
+	}
+}
+
+func TestApiKeyFromDocs(t *testing.T) {
+	os.Setenv("CRONITOR_API_KEY", "<api key>")
+	buffer := new(bytes.Buffer)
+	RootCmd.SetOut(buffer)
+	err := RootCmd.Execute()
+	// we expect this to fail
+	if err == nil {
+		t.Error("Gave '<api key>' as key, should have returned an error to the user")
+	}
+}
+
+func TestInvalidApiKey(t *testing.T) {
+	os.Setenv("CRONITOR_API_KEY", "k����]}�x�M�k�w��5{���\u0378")
+	buffer := new(bytes.Buffer)
+	RootCmd.SetOut(buffer)
+	err := RootCmd.Execute()
+	// we expect this to fail
+	if err == nil {
+		t.Error("Invalid API key provided; somehow RootCmd executed anyway")
 	}
 }
